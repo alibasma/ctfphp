525054a88a5c317c28558fda3346edc2f63282ed
alibasma <114940530+alibasma@users.noreply.github.com>
2025-04-05 17:47:38 +0200

    Update lire_lang.php

--- lire_lang.php
+++ lire_lang.php
@@ -1,12 +1,5 @@
 <?php
-// Spécifie le chemin du fichier
-$file = 'index.php';
 
-// Vérifie si le fichier existe
-if (file_exists($file)) {
-    // Affiche le contenu du fichier avec coloration syntaxique
-    highlight_file($file);
-} else {
-    echo "Le fichier $file n'existe pas.";
-}
+
+system("cat index.php");
 ?>
